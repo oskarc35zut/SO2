f446aa5aec7791498c277d9b704c340ef8c710cb
Surface - linux <oskarc35@gmail.com>
2017-04-22 12:42:35 +0200

    lab3 0.999999

--- Zad3/errest.c
+++ Zad3/errest.c
@@ -28,9 +28,25 @@ int main( int argc, char **argv)
 
 	if((length%2) != 0 && (length) != 1) {length--;}
 	
-	char* ar2;
-	char bufor[30];
+	char *ar2 = NULL;
+	//char *bufor = NULL;	
+	char bufor[1000];
+	strcat(bufor," ");
 
+			if(argc == 3)
+			{
+
+				strcat(bufor, argv[1]);
+				strcat(bufor," ");
+				//strcat(bufor, argv[1]);
+				strcat(bufor, argv[2]);
+				strcat(bufor," ");
+			}
+			if(argc == 2)
+			{
+				strcat(bufor, argv[1]);
+			}
+	
 
 	if(length > 1)
 		{
@@ -41,37 +57,36 @@ int main( int argc, char **argv)
 		for(int i = 0; i < (length/2); i++) input_two[i] = input[i+((length/2))];
 		
 		pid_t pID1 = fork();
+
 		if(pID1 == 0)
 			{
 
-			if(argc > 2)
-			{
-				ar2 = argv[2];
-				strcat(bufor, ar2);
-			}
-			strcat(bufor," ");
+
+
 			const char* in = input_one;
-			strcat(bufor, in);
-			execlp("./errest", "errest", input_one , 4444, NULL);
+			strcat(bufor, argv[1]);
+			
+
+
+			execlp("./errest", "errest", input_one , bufor, NULL);
 			}
-		if(pID1 > 0)
+			if(pID1 > 0)
 			{
 			
 			
 			wait(0);
 			}
 
-		pid_t pID2 = fork();
-		if(pID2 == 0)
+			pid_t pID2 = fork();
+
+			if(pID2 == 0)
 			{
-			//his = strcpy(argv[2],input_two);
+			
 			execlp("./errest", "errest", input_two , NULL);
 			}
-		if(pID2 > 0)
+			if(pID2 > 0)
 			{
-			//if (strlen(input) == 1)
-{
-}
+
 			wait(0);
 			}
 		
@@ -82,6 +97,6 @@ int main( int argc, char **argv)
 
 
 	
-	printf( "%s %s \n",bufor, input);
+	printf( "%s \n",bufor);
 	return 0;
 	}
